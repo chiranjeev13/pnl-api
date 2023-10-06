9d041a2ff0d4dd4d1e7c40466c23d2f97791459e
chiranjeev13 <chiranjeevmishra13@gmail.com>
2023-10-07 02:34:04 +0530

    added mint sales purchases

--- pages/api/nft.js
+++ pages/api/nft.js
@@ -1,107 +1,207 @@
-import Web3 from "web3";
-import BigNumber from "bignumber.js";
 import { Alchemy, Network } from "alchemy-sdk";
+
 const config = {
-    apiKey: "TMbUqfFKq008ZyBmPSc6gLKw1s-oUQnX",
-    network: Network.ETH_MAINNET,
+  apiKey: "TMbUqfFKq008ZyBmPSc6gLKw1s-oUQnX",
+  network: Network.ETH_MAINNET,
 };
 
-export default async function handler(req, res) {
-    const { method } = req;
+async function getSales(wallet, token, alchemy) {
+  const SELL = [];
+  const ERC20 = [];
+  const sells = await alchemy.core.getAssetTransfers({
+    toBlock: "latest",
+    fromAddress: wallet,
+    contractAddresses: [token],
+    withMetadata: true,
+    category: ["ERC721", "ERC1155", "ERC20"],
+  });
 
-    const { wallet } = req.query;
+  await Promise.all(
+    sells.transfers.map(async (sold) => {
+      const txhash = sold.hash;
+      const tx = await alchemy.transact.getTransaction(txhash);
+      const block = await alchemy.core.getBlock(tx.blockNumber);
+      const timestamp = block.timestamp;
+      const value = parseInt(tx.value._hex, 16) / 10 ** 18;
+      const recpt = await alchemy.core.getTransactionReceipt(txhash);
+      const res = recpt.logs.filter((transaction) => {
+        return transaction.topics.includes(
+          "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+        );
+      });
 
-    const { token } = req.query;
-
-
-    const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
-    const TRANSFER_EVENT_SIGNATURE = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef';
-
-
-
-    switch (method) {
-        case "GET":
-            const web3 = new Web3(new Web3.providers.HttpProvider('https://eth-mainnet.g.alchemy.com/v2/TMbUqfFKq008ZyBmPSc6gLKw1s-oUQnX'));
-            const alchemy = new Alchemy(config);
-
-            const sells = await alchemy.core.getAssetTransfers({
-                toBlock: "latest",
-                fromAddress: wallet,
-                contractAddresses: [token],
-                withMetadata: true,
-                category: ["erc721"],
-            })
-
-            // const sellTxns = sells.transfers.map(async (txn) => {
-            //     const txHash = txn.hash
-
-            //     const txnReceipt = await alchemy.core.getTransactionReceipt(txHash)
-
-            //     const trade = {
-            //         txHash,
-            //         wethAmount: 0,
-            //         tokenAmount: 0,
-            //     }
-
-            //     for (const log of txnReceipt.logs) {
-            //         if (log.topics[0] === TRANSFER_EVENT_SIGNATURE) {
-            //             // Decode the ERC-20 Transfer event
-            //             const from = '0x' + log.topics[1].slice(26);  // Topics are 32 bytes; Ethereum addresses are 20 bytes
-            //             const to = '0x' + log.topics[2].slice(26);
-            //             const token = log.address;
-            //             const amount = web3.utils.fromWei(web3.utils.toBigInt(log.data), 'ether')  // Amount is stored in the log data
-
-            //             // only count the amount if the to address is not the wallet address
-
-            //             // if (from.toLowerCase() !== wallet.toLowerCase()) return;
-
-            //             if (token.toLowerCase() === WETH_ADDRESS.toLowerCase()) {
-            //                 trade.wethAmount = amount
-            //             } else {
-            //                 trade.tokenAmount = amount
-            //             }
-            //         }
-            //     }
-
-            //     return trade
-
-
-
-            // })
-
-
-            const buys = await alchemy.core.getAssetTransfers({
-                toBlock: "latest",
-                toAddress: wallet,
-                contractAddresses: [token],
-                withMetadata: true,
-                category: ["erc721"],
-            })
-
-            const buyTxns = buys.transfers.map(async (txn) => {
-                const txHash = txn.hash
-                // hash already exists, skip it 
-
-                const txnReceipt = await alchemy.core.getTransactionReceipt(txHash)
-                console.log(txnReceipt)
-
-
-            })
-
-
-
-
-            res.status(200).json({
-                buys,
-                sells,
-                buyTxns,
-
+      await Promise.all(
+        res.map(async (tken) => {
+          if (tken.address !== token) {
+            const tokenmeta = await alchemy.core.getTokenMetadata(tken.address);
+            const decimals = tokenmeta.decimals;
+            const logo = tokenmeta.logo;
+            const symbol = tokenmeta.symbol;
+            const value = parseInt(tken.data) / 10 ** decimals;
+            ERC20.push({
+              symbol: symbol,
+              logo: logo,
+              value: value,
             });
+          }
+        })
+      );
 
-            break;
-        default:
-            res.setHeader("Allow", ["GET"]);
-            res.status(405).end(`Method ${method} Not Allowed`);
-    }
+      SELL.push({
+        tokenId: parseInt(sold.tokenId, 16),
+        txhash: txhash,
+        value: value,
+        timestamp: timestamp,
+        ERC20: ERC20,
+      });
+    })
+  );
 
+  return SELL;
+}
+
+async function getMints(wallet, token, alchemy) {
+  const MINTS = [];
+  const ERC20 = [];
+
+  const nftdata = {
+    contractAddresses: [token],
+    tokenType: ["ERC721", "ERC1155", "ERC20"],
+  };
+
+  const mints = await alchemy.nft.getMintedNfts(wallet, nftdata);
+
+  await Promise.all(
+    mints.nfts.map(async (nft) => {
+      const txhash = nft.transactionHash;
+      const tx = await alchemy.transact.getTransaction(txhash);
+      const block = await alchemy.core.getBlock(tx.blockNumber);
+      const timestamp = block.timestamp;
+      const value = parseInt(tx.value._hex, 16) / 10 ** 18;
+      const recpt = await alchemy.core.getTransactionReceipt(txhash);
+      const res = recpt.logs.filter((transaction) => {
+        return transaction.topics.includes(
+          "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+        );
+      });
+      await Promise.all(
+        res.map(async (tken) => {
+          if (tken.address !== token) {
+            const tokenmeta = await alchemy.core.getTokenMetadata(tken.address);
+            const decimals = tokenmeta.decimals;
+            const logo = tokenmeta.logo;
+            const symbol = tokenmeta.symbol;
+            const value = parseInt(tken.data) / 10 ** decimals;
+            ERC20.push({
+              symbol: symbol,
+              logo: logo,
+              value: value,
+            });
+          }
+        })
+      );
+
+      MINTS.push({
+        tokenId: nft.tokenId,
+        txhash: nft.transactionHash,
+        value: value,
+        timestamp: timestamp,
+        ERC20: ERC20,
+      });
+    })
+  );
+
+  return MINTS;
+}
+
+async function getPurchases(wallet, token, alchemy) {
+  const BUY = [];
+  const ERC20 = [];
+
+  const buys = await alchemy.core.getAssetTransfers({
+    toBlock: "latest",
+    toAddress: wallet,
+    contractAddresses: [token],
+    withMetadata: true,
+    category: ["ERC721", "ERC1155", "ERC20"],
+  });
+
+  await Promise.all(
+    buys.transfers.map(async (txn) => {
+      const txHash = txn.hash;
+      console.log(txn.from);
+      if (txn.from !== "0x0000000000000000000000000000000000000000") {
+        const tx = await alchemy.transact.getTransaction(txHash);
+        const block = await alchemy.core.getBlock(tx.blockNumber);
+        const recpt = await alchemy.core.getTransactionReceipt(txHash);
+        const res = recpt.logs.filter((transaction) => {
+          return transaction.topics.includes(
+            "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef"
+          );
+        });
+        await Promise.all(
+          res.map(async (tken) => {
+            if (tken.address !== token) {
+              const tokenmeta = await alchemy.core.getTokenMetadata(
+                tken.address
+              );
+              const decimals = tokenmeta.decimals;
+              const logo = tokenmeta.logo;
+              const symbol = tokenmeta.symbol;
+              const value = parseInt(tken.data) / 10 ** decimals;
+              ERC20.push({
+                symbol: symbol,
+                logo: logo,
+                value: value,
+              });
+            }
+          })
+        );
+
+        const timestamp = block.timestamp;
+        const value = parseInt(tx.value._hex, 16) / 10 ** 18;
+        BUY.push({
+          tokenId: parseInt(txn.tokenId, 16),
+          txhash: txHash,
+          value: value,
+          timestamp: timestamp,
+          ERC20: ERC20,
+        });
+      }
+    })
+  );
+
+  return BUY;
+}
+
+export default async function handler(req, res) {
+  const { method } = req;
+  const { wallet } = req.query;
+  const { token } = req.query;
+
+  switch (method) {
+    case "GET":
+      const alchemy = new Alchemy(config);
+      try {
+        const SELL = await getSales(wallet, token, alchemy);
+        const MINTS = await getMints(wallet, token, alchemy);
+        const BUY = await getPurchases(wallet, token, alchemy);
+
+        const Analysis = {
+          Sales: SELL,
+          Mints: MINTS,
+          Purchases: BUY,
+        };
+        res.status(200).json({
+          Analysis,
+        });
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "An error occurred." });
+      }
+      break;
+    default:
+      res.setHeader("Allow", ["GET"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
 }
